2a7cf9a0cc2cd3b1c6b67e6c5773edef3aca43d8
Ed George <ed.george@himumsaiddad.com>
2017-12-20 13:05:59 +0000

    Update .gitignore to include Gradle Wrapper jar

--- gradle/buildViaTravis.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# This script will build the project.
-
-echo -e 'Build Branch for Release => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']'
-./gradlew -Prelease.travisci=true -Prelease.useLastTag=true
